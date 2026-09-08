f1dfaa32af2501f6bf340a24592089f85ea684a8
Dhanush <dhanushsp616@gmail.com>
2026-09-08 19:40:28 +0530

    Updates

--- assets/react-vendor-BqcB7zkW.js
+++ assets/react-vendor-BqcB7zkW.js
@@ -1,4 +1,4 @@
-import{r as _a}from"./icons-vendor-BPLuHTjZ.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
+import{r as _a}from"./icons-vendor-FtXIttfV.js";var Co={exports:{}},ve={},xo={exports:{}},_o={};/**
  * @license React
  * scheduler.production.min.js
  *
